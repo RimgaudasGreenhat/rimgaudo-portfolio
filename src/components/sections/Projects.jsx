d1a0ffe7e84c27b4e81053a71125810ce7277205
RimgaudasGreenhat <rimgaudasAER@gmail.com>
2025-10-27 20:32:41 +0200

    portfolioAprašymuUpdate

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,13 +14,12 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold mb-2"> Iksiukai-Nuliukai</h3>
               <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
+                Paprastas iksiukų nuliukų, žaidimas pasinaudojant React-Vite ir CSS norint pasimokinti React pagrindų.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                {["React", "CSS"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -49,13 +48,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <h3 className="text-xl font-bold mb-2">Wordyle</h3>
               <p className="text-gray-400 mb-4">
-                ML-powered data visualization platform with predictive analytics
-                and interactive reports.
+                Wordly žaidimo kopija kurioje nėra ,,dienos žodžio'' ir taip galima žaisti kiek nori.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                {["React", "CSS"].map((tech, key) => (
                   <span
                     key={key}
                     className="
